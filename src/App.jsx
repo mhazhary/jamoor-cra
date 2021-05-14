32e524150babeb73832bb529349941aa576692ae
M. Hanif Azhary <hanif.hz@gmail.com>
2021-05-14 22:01:47 +0000

    test with ApexCharts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import GaugeChart from 'react-gauge-chart';
+import ReactApexChart from 'react-apexcharts';
 import logo from './logo.svg';
 import './App.css';
 
@@ -72,6 +73,88 @@ function App() {
   const valueV1 = parseFloat(valV1) / 100;
   const valueV2 = parseFloat(valV2) / 100;
   const valueV3 = parseFloat(valV3) / 100;
+  const ApexChart = {
+    series: [valueV1],
+    options: {
+      chart: {
+        height: 350,
+        type: 'radialBar',
+        toolbar: {
+          show: true,
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 225,
+          hollow: {
+            margin: 0,
+            size: '70%',
+            background: '#fff',
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: 'front',
+            dropShadow: {
+              enabled: true,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24,
+            },
+          },
+          track: {
+            background: '#fff',
+            strokeWidth: '67%',
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35,
+            },
+          },
+
+          dataLabels: {
+            show: true,
+            name: {
+              offsetY: -10,
+              show: true,
+              color: '#888',
+              fontSize: '17px',
+            },
+            value: {
+              formatter(val) {
+                return parseInt(val, 10);
+              },
+              color: '#111',
+              fontSize: '36px',
+              show: true,
+            },
+          },
+        },
+      },
+      fill: {
+        type: 'gradient',
+        gradient: {
+          shade: 'dark',
+          type: 'horizontal',
+          shadeIntensity: 0.5,
+          gradientToColors: ['#ABE5A1'],
+          inverseColors: true,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100],
+        },
+      },
+      stroke: {
+        lineCap: 'round',
+      },
+      labels: ['Percent'],
+    },
+
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -94,6 +177,11 @@ function App() {
       <p className="text-pink-600">
         {isHardwareConnected}
       </p>
+      <div id="card">
+        <div id="chart">
+          <ReactApexChart options={ApexChart.options} series={ApexChart.series} type="radialBar" height={350} />
+        </div>
+      </div>
       <div className="md:grid grid-cols-3 bg-gray-900">
         <div className="flex-1">
           <GaugeChart
